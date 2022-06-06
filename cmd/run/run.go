52ceb8a595a3e8caf603ec167d52dd66532b9d27
auual <ding@ibyte.me>
2022-06-06 16:54:59 +0800

    add: running search mode.

--- cmd/run/run.go
+++ cmd/run/run.go
@@ -42,6 +42,7 @@ const (
 	$ ./woodpecker run --dir=/Users/ding/desktop/woodpecker/ --mode=md5 --code=81129dsxxxxx2d8123
 
 	Search according to different patterns 👇
+	
 	$ ./woodpecker run --dir=/Users/ding/desktop/woodpecker/ --mode=hex --code=74 63 61 73 63 61 6e 2f --out=result.json
 	`
 )
